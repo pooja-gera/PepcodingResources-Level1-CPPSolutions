136b943d9d2f12f6788ffd8a3b36511a9d192045
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2021-02-12 14:31:00 +0530

    Solved print decreasing.

---
+++ IntroductionToRecursion_PrintDecreasing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printDecreasing(int n){
+    // write your code here
+    if(n==1){
+        cout<<1;
+        return;
+    }
+    cout<<n<<endl;
+    return printDecreasing(n-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    printDecreasing(n);
+}
